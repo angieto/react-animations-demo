953864dc62852a0577744f79cf7a89000a60b0f7
Angie To <angie.to@berkeley.edu>
2019-01-04 16:50:12 -0800

    added transition component to modal

--- src/App.js
+++ src/App.js
@@ -43,8 +43,12 @@ class App extends Component {
               }} />
           )}
         </Transition> 
-        { this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null }
-        { this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null }
+        <Transition in={this.state.modalIsOpen} timeout={300} mountOnEnter unmountOnExit>
+          {state => (
+            <Modal show={state} closed={this.closeModal}/>
+          )}
+        </Transition>
+        { this.state.modalIsOpen ? <Backdrop show /> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
