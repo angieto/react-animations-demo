4d24a1b6b06fa905f74f6df5db8a4bfadef4ef1d
Angie To <angie.to@berkeley.edu>
2019-01-04 16:28:18 -0800

    used the state property of react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -22,8 +24,27 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen} />
+        <button 
+              className="Button" 
+              onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))} >
+          Toggle
+        </button>
+        <br />
+        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+          {state => (
+            <div 
+              style={{
+                backgroundColor: 'salmon', 
+                width: 100, 
+                height: 100, 
+                margin: 'auto', 
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1 // entered, entering, exited, exiting
+              }} />
+          )}
+        </Transition> 
+        { this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null }
+        { this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
